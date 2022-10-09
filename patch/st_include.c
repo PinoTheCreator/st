b09f84d250168fb176f1a7d5ca1adc50c7ed1af8
Carlo Aspren Gines Lara <101257700+PinoTheCreator@users.noreply.github.com>
2022-10-10 00:24:48 +0200

    first commit updated to last flexipatch commit (maybe the last useful)

---
+++ patch/st_include.c
@@ -0,0 +1,6 @@
+/* Patches */
+#include "copyurl.c"
+#include "iso14755.c"
+#include "scrollback.c"
+#include "sixel_st.c"
+#include "sync.c"
---
+++ patch/st_include.h
@@ -0,0 +1,10 @@
+/* Patches */
+#include "copyurl.h"
+#include "iso14755.h"
+#include "openurlonclick.h"
+#include "scrollback.h"
+#include "sixel_st.h"
+#include "sync.h"
+// #if VIM_BROWSE_PATCH
+// #include "normalMode.h"
+// #endif
